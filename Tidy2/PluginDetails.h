485ca7520da120227e632e4f4288a3c6d89ace43
kazukd <83990212+kazukd@users.noreply.github.com>
2021-08-17 11:42:20 +1200

    First version

---
+++ Tidy2/PluginDetails.h
@@ -0,0 +1,14 @@
+
+#define PLUGIN_VERSION_COMMA		0,2,1,0
+
+#define PLUGIN_VERSION_COMMA_TEXT   "0,2,1,0"
+
+#define PLUGIN_VERSION_POINT        "0.2.1.0"
+
+#define PLUGIN_NAME					"Tidy2-x64"
+
+#define PLUGIN_COPYRIGHT			"(C) 2021 Kazuhisa Kondo"
+
+#define PLUGIN_AUTHOR				"Kazuhisa Kondo"
+
+#define PLUGIN_LICENSE				"License GPLv3"
